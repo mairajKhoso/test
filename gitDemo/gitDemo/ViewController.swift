b450234f5bb66395ba8c0d8ecdd7e9ae976fd533
Mairaj khoso <mairajkhosok13@gmail.com>
2023-01-01 17:57:29 +0500

    Fixes in AppDelegate

--- gitDemo/gitDemo/ViewController.swift
+++ gitDemo/gitDemo/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Testing it
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
